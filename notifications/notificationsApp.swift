c5a392ebcc5f74b563408bf57e9fc98c1a47a0b6
Fathima Shakki K C <fathimashakki21@gmail.com>
2025-05-23 15:57:55 +0530

    Initial Commit

---
+++ notifications/notificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  notificationsApp.swift
+//  notifications
+//
+//  Created by Shaan on 23/05/25.
+//
+
+import SwiftUI
+
+@main
+struct notificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
